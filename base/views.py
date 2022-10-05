25226418c3b71ca130b80a65d9c5774a622ae996
yijing-wu <yijingwu.w@gmail.com>
2022-10-05 12:22:25 -0400

    Pass variables into Templates, render templates

--- base/views.py
+++ base/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me '},
+    {'id': 3, 'name': 'Frontend developers'}
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    context =  {'rooms': rooms}
+    return render(request, "home.html", context)
 
 
 def room(request):
