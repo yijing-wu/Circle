36487d479c88c97f93b3887c35c31c42935b8ded
yijing-wu <yijingwu.w@gmail.com>
2022-10-05 10:53:08 -0400

    Create Template and render HTML page

--- base/views.py
+++ base/views.py
@@ -1,13 +1,11 @@
-import imp
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Home page")
+    return render(request, "home.html")
 
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, "room.html")
