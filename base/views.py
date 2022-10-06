f3deb10af6f43302a0ec8d9f7874fac95728b179
yijing-wu <yijingwu.w@gmail.com>
2022-10-06 11:43:50 -0400

    Only show the searched/room related activity in home page

--- base/views.py
+++ base/views.py
@@ -83,7 +83,7 @@ def home(request):
     # use Q to link multiple search conditions
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {
         "rooms": rooms,
