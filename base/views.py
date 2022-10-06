e323ba25ea18d7cc30333fedf13138385c1e6ef6
yijing-wu <yijingwu.w@gmail.com>
2022-10-06 10:27:46 -0400

    User registeration

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from django.contrib import messages
@@ -5,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 
 from .forms import RoomForm
@@ -20,13 +22,15 @@ from .forms import RoomForm
 
 def loginPage(request):
 
+    page = "login"
+
     # if user already login, will redirect to home page when trying to enter login page
     if request.user.is_authenticated:
         return redirect("home")
 
     if request.method == "POST":
         # get value from frontend
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         # check whether user exist
@@ -43,13 +47,31 @@ def loginPage(request):
         else:
             messages.error(request, "Username OR Password does not exist.")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)  # after register, login user
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during registeration.")
+
+    context = {"form": form}
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
