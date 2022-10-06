22338b54b8e0b7ee8533a5a857613137253d724f
yijing-wu <yijingwu.w@gmail.com>
2022-10-05 23:05:35 -0400

    Rename Project

--- hobbycircle/asgi.py
+++ hobbycircle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for studygroup project.
+ASGI config for hobbycircle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studygroup.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hobbycircle.settings")
 
 application = get_asgi_application()
