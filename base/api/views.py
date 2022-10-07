0afe7ddc97b9b578f6270f0f6379f33e88a8c1ea
yijing-wu <yijingwu.w@gmail.com>
2022-10-07 16:36:51 -0400

    Add REST API: get rooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
+from base.api import serializers
 
 
 @api_view(["GET"])
@@ -10,3 +13,13 @@ def getRoutes(request):
         "GET /api/rooms/:id",
     ]
     return Response(routes)
+
+
+@api_view(["GET"])
+def getRooms(request):
+    rooms = Room.objects.all()
+    # we get python list query set here,
+    # need to serialize, turn python objects into JSON objects
+    serializer = RoomSerializer(rooms, many=True)
+    # many means we what to serialize many objects
+    return Response(serializer.data)
