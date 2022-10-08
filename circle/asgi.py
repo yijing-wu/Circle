7a31afe93933f929044840fa42d5f82928304e31
yijing-wu <yijingwu.w@gmail.com>
2022-10-08 13:20:03 -0400

    Rename and fix small issues

--- circle/asgi.py
+++ circle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hobbycircle project.
+ASGI config for circle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hobbycircle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "circle.settings")
 
 application = get_asgi_application()
--- circle/wsgi.py
+++ circle/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hobbycircle project.
+WSGI config for circle project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hobbycircle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "circle.settings")
 
 application = get_wsgi_application()
