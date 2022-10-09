2f9f901047597f7d579693e64250f7d7b9a22c51
yijing-wu <yijingwu.w@gmail.com>
2022-10-08 21:05:22 -0400

    Change to Postgresql database of Railway

--- circle/settings.py
+++ circle/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "circle.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"],
     }
 }
 
