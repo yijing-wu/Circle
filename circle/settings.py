20976ff5a962513d99f9bc7c8d6905cb7e3e26f5
yijing-wu <yijingwu.w@gmail.com>
2022-12-08 22:10:46 -0500

    Add csrf_trusted_origins

--- circle/settings.py
+++ circle/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = ["https://circle.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://circle.up.railway.app"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
